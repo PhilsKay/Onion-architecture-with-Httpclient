2124c29f301d42b4fed62565b9f2e12ef583ecae
Philip Kayode <philipinhokayode@gmail.com>
2024-01-15 14:08:16 +0100

    Add project files.

---
+++ Controllers/FinanceController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Onion_architecture.Infrastructure.RecipesAPI;
+
+namespace Onion_architecture.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinanceController : ControllerBase
+    {
+        private readonly IFinanceNews _financeNews;
+        public FinanceController(IFinanceNews financeNews)
+        {
+            _financeNews = financeNews;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetNews()
+        {
+            var news = await _financeNews.GetNews();
+            return Ok(news);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Onion_architecture.Infrastructure.FinanceAPI;
+using Onion_architecture.Infrastructure.RecipesAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient("Finance-news",option =>
+{
+    option.BaseAddress = new Uri("https://api.apilayer.com/");
+    option.DefaultRequestHeaders.Add("apikey", builder.Configuration["FinanceApiKey"]);
+});
+
+builder.Services.AddTransient<IFinanceNews, FinanceNewsService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
